6721a14d9078b633c20f3c498cdd74f3604f145a
Buğrahan Arslan <ciyotin@gmail.com>
2021-01-03 19:51:28 +0300

    menü oldu gibi gidişler vs okey, dosya türlerine erişimde sıkıntı oluyor

---
+++ func.py
@@ -0,0 +1,242 @@
+import sys, os
+import curses
+import subprocess
+from math import *
+import socket
+
+def pwd():
+    firstPwd = subprocess.check_output(['pwd']).decode('utf-8').strip('\n')
+    return firstPwd
+
+def lsP(path):
+    
+    output_ls=subprocess.check_output(['ls', path+"/.."]).decode('utf-8')
+    return output_ls.splitlines()
+
+def ls(path):
+    
+    output_ls=subprocess.check_output(['ls', path]).decode('utf-8')
+    return output_ls.splitlines()
+
+""" def ls():
+    
+    output_ls=subprocess.check_output(['ls']).decode('utf-8')
+    return output_ls.splitlines()
+     """
+
+
+def refreshChild(boxChild,ourPwd,stdscr,selectedDir): #if os.path.isdir(path):
+    boxChild.erase()
+    height, width = stdscr.getmaxyx()
+
+    start_x = int((width // 2))
+    start_y = int((height // 2) - 2)
+    maxCol=width // 3-3
+    max_row = height-5 #max number of rows
+
+    highlightText = curses.color_pair( 2 )
+    normalText = curses.A_NORMAL
+
+    childPwd='/'
+    # / değilse / koy
+    if ourPwd is "/":
+        childPwd = ourPwd + selectedDir 
+        
+    else:
+        childPwd = ourPwd +"/"+selectedDir
+    #todo: Detaylı şekilde olacak.
+    if  os.path.isfile(childPwd) or not os.access(childPwd, os.X_OK):
+        boxChild.addstr( 1, 1, "Cannot Access", highlightText )
+        boxChild.border( 0 )
+        stdscr.refresh()
+        boxChild.refresh()
+        return -1
+    list_ls = ls(childPwd)
+    strings = list_ls
+    row_num = len( strings )
+    position = 1
+
+    
+
+    
+
+    for i in range( 1, max_row  + 1 ):
+        if row_num == 0:
+            boxChild.addstr( 1, 1, "No Files or Dirs inside", highlightText )
+        else:
+            if (i == position):
+                boxChild.addstr( i, 2, str( i ) + " - " + strings[ i - 1 ], highlightText )
+            else:
+                boxChild.addstr( i, 2, str( i ) + " - " + strings[ i - 1 ], normalText )
+            if i == row_num:
+                break
+    
+    boxChild.border( 0 )
+    stdscr.refresh()
+    boxChild.refresh()
+    return position
+
+
+def refreshParent(boxParent,ourPwd,stdscr):
+    boxParent.erase()
+    height, width = stdscr.getmaxyx()
+
+    start_x = int((width // 2))
+    start_y = int((height // 2) - 2)
+    maxCol=width // 3-3
+    max_row = height-5 #max number of rows
+
+    list_ls = lsP( ourPwd )
+    strings = list_ls
+    row_num = len( strings )
+    pPosition = 0
+
+    highlightText = curses.color_pair( 2 )
+    normalText = curses.A_NORMAL
+
+    for i in range( 1, max_row  + 1 ):
+        if row_num == 0:
+            boxParent.addstr( 1, 1, "There aren't strings", highlightText )
+        else:
+            if strings[i -1] == ourPwd.split('/')[-1]: #l
+                #boxParent.addstr( i, 2, str( i ) + " - " + strings[ i - 1 ], highlightText )
+                boxParent.addstr( i, 2, str( i ) + " - " + ourPwd.split('/')[-1], highlightText )
+                pPosition = i
+            else:
+                boxParent.addstr( i, 2, str( i ) + " - " + strings[ i - 1 ], normalText )
+            #boxParent.addstr( 20, 2, str( 61 ) + " - " + ourPwd.split('/')[-1], highlightText )
+            if i == row_num:
+                break
+    
+    boxParent.border( 0 )
+    stdscr.refresh()
+    boxParent.refresh()
+    return pPosition
+
+
+def drawChild(stdscr, selectedDir): #if os.path.isdir(path):
+
+    height, width = stdscr.getmaxyx()
+
+    start_x = int((width))
+    start_y = int((height // 2) - 2)
+    maxCol=width // 3-3
+    max_row = height-5 #max number of rows
+    boxChild = curses.newwin( max_row + 2, maxCol, 1, start_x - maxCol-2 )
+    boxChild.box()
+
+    highlightText = curses.color_pair( 2 )
+    normalText = curses.A_NORMAL
+    
+    childPwd='/'
+    if pwd() is "/":
+        childPwd = pwd() + selectedDir 
+        
+    else:
+        childPwd = pwd() +"/"+selectedDir
+    
+    list_ls = ls(childPwd)
+    strings = list_ls
+    row_num = len( strings )
+    
+    #boxChild.addstr( 25, 2, str( 61 ) + " - " + pwd()+selectedDir, highlightText )
+    if  os.path.isfile(childPwd) or not os.access(childPwd, os.X_OK):
+        boxChild.addstr( 1, 1, "Cannot Access", highlightText )
+        boxChild.border( 0 )
+        stdscr.refresh()
+        boxChild.refresh()
+        return boxChild
+
+   
+    pages = int( ceil( row_num / max_row ) )
+    position = 1
+    page = 1
+    
+    for i in range( 1, max_row  + 1 ):
+        if row_num == 0:
+            boxChild.addstr( 1, 1, "There aren't strings", highlightText )
+        else:
+            if (i == position):
+                boxChild.addstr( i, 2, str( i ) + " - " + strings[ i - 1 ], highlightText )
+            else:
+                boxChild.addstr( i, 2, str( i ) + " - " + strings[ i - 1 ], normalText )
+            if i == row_num:
+                break
+
+    #stdscr.refresh()
+    boxChild.refresh()
+   
+    #boxParent.erase()
+    #stdscr.border( 0 )
+    boxChild.border( 0 )
+
+    return boxChild
+
+
+    # stdscr.refresh()
+    # boxParent.refresh()
+
+
+
+
+#######################################
+
+def drawParent(stdscr): #sol menü 
+
+    height, width = stdscr.getmaxyx()
+
+    start_x = int((width // 2))
+    start_y = int((height // 2) - 2)
+    maxCol=width // 3-3
+    max_row = height-5 #max number of rows
+    boxParent = curses.newwin( max_row + 2, maxCol, 1, 1 )
+    boxParent.box()
+
+    
+    highlightText = curses.color_pair( 2 )
+    normalText = curses.A_NORMAL
+
+    list_ls = lsP(pwd())
+    strings = list_ls
+    row_num = len( strings )
+    
+    
+    
+
+    statusbarstr = "Press 'q' to exit | STATUS BAR "
+    # Render status bar
+    stdscr.attron(curses.color_pair(3))
+    stdscr.addstr(height-1, 0, statusbarstr)
+    stdscr.addstr(height-1, len(statusbarstr), " " * (width - len(statusbarstr) - 1))
+    stdscr.attroff(curses.color_pair(3))
+    
+   
+    pages = int( ceil( row_num / max_row ) )
+    position = 1
+    page = 1
+    
+    for i in range( 1, max_row  + 1 ):
+        if row_num == 0:
+            boxParent.addstr( 1, 1, "There aren't strings", highlightText )
+        else:
+            if strings[i -1] == pwd().split('/')[-1]: #l
+                #boxParent.addstr( i, 2, str( i ) + " - " + strings[ i - 1 ], highlightText )
+                boxParent.addstr( i, 2, str( i ) + " - " + pwd().split('/')[-1], highlightText )
+            else:
+                boxParent.addstr( i, 2, str( i ) + " - " + strings[ i - 1 ], normalText )
+            if i == row_num:
+                break
+
+    #stdscr.refresh()
+    boxParent.refresh()
+   
+    #boxParent.erase()
+    #stdscr.border( 0 )
+    boxParent.border( 0 )
+
+    return boxParent
+
+
+    # stdscr.refresh()
+    # boxParent.refresh()
+    
--- main.py
+++ main.py
@@ -1,136 +1,7 @@
-import sys, os
-import curses
-import subprocess
-from math import *
+from func import *
 
 
-def pwd():
-    firstPwd = subprocess.check_output(['pwd']).decode('utf-8').strip('\n')
-    return firstPwd
-
-def lsP(path):
-    
-    output_ls=subprocess.check_output(['ls', path+"/.."]).decode('utf-8')
-    return output_ls.splitlines()
-
-def ls(path):
-    
-    output_ls=subprocess.check_output(['ls', path]).decode('utf-8')
-    return output_ls.splitlines()
-
-""" def ls():
-    
-    output_ls=subprocess.check_output(['ls']).decode('utf-8')
-    return output_ls.splitlines()
-     """
-
-
-
-def drawParent(stdscr):
-
-    height, width = stdscr.getmaxyx()
-
-    start_x = int((width // 2))
-    start_y = int((height // 2) - 2)
-    maxCol=width // 3-3
-    max_row = height-5 #max number of rows
-    boxParent = curses.newwin( max_row + 2, maxCol, 1, 1 )
-    boxParent.box()
-
-    
-    highlightText = curses.color_pair( 2 )
-    normalText = curses.A_NORMAL
-
-    list_ls = lsP(pwd())
-    strings = list_ls
-    row_num = len( strings )
-    
-    
-    
-
-    statusbarstr = "Press 'q' to exit | STATUS BAR "
-    # Render status bar
-    stdscr.attron(curses.color_pair(3))
-    stdscr.addstr(height-1, 0, statusbarstr)
-    stdscr.addstr(height-1, len(statusbarstr), " " * (width - len(statusbarstr) - 1))
-    stdscr.attroff(curses.color_pair(3))
-    
-   
-    pages = int( ceil( row_num / max_row ) )
-    position = 1
-    page = 1
-    
-    for i in range( 1, max_row  + 1 ):
-        if row_num == 0:
-            boxParent.addstr( 1, 1, "There aren't strings", highlightText )
-        else:
-            if strings[i -1] == pwd().split('/')[-1]: #l
-                #boxParent.addstr( i, 2, str( i ) + " - " + strings[ i - 1 ], highlightText )
-                boxParent.addstr( i, 2, str( i ) + " - " + pwd().split('/')[-1], highlightText )
-            else:
-                boxParent.addstr( i, 2, str( i ) + " - " + strings[ i - 1 ], normalText )
-            if i == row_num:
-                break
-
-    stdscr.refresh()
-    boxParent.refresh()
-   
-    #boxParent.erase()
-    #stdscr.border( 0 )
-    boxParent.border( 0 )
-
- 
-
-
-    # stdscr.refresh()
-    # boxParent.refresh()
-    
-
-def draw_menu(stdscr):
-    k = 0
-    cursor_x = 0
-    cursor_y = 0
-
-    # Clear and refresh the stdscr for a blank canvas
-    stdscr.clear()
-    stdscr.refresh()
-
-    stdscr = curses.initscr()
-    curses.noecho()
-    curses.cbreak()
-    curses.start_color()
-    stdscr.keypad( 1 )
-    curses.init_pair(1,curses.COLOR_BLACK, curses.COLOR_CYAN)
-    #stdscr.border( 0 )
-    curses.curs_set( 0 )
-
-    # Start colors in curses
-    curses.start_color()
-    curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLUE)
-    curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_BLUE)
-    curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_GREEN)
-
-    # Loop where k is the last character pressed
-    #while (k != ord('q')):
-
-    # Initialization
-    stdscr.clear()
-
-
-    
-    drawPwdLs(stdscr)
-    
-    
-    # Refresh the stdscr
-    #stdscr.refresh()
-
-    # Wait for next input
-    #k = stdscr.getch()
-
-
-
-
-def drawPwdLs(stdscr):
+def drawPwdLs(stdscr): #orta menü
     height, width = stdscr.getmaxyx()
 
     start_x = int((width // 2))
@@ -139,7 +10,7 @@ def drawPwdLs(stdscr):
     max_row = height-5 #max number of rows
     box = curses.newwin( max_row + 2, maxCol, 1, start_x - maxCol//2 )
     box.box()
-    drawParent(stdscr)
+    boxParent=drawParent(stdscr)
     
     highlightText = curses.color_pair( 2 )
     normalText = curses.A_NORMAL
@@ -147,14 +18,23 @@ def drawPwdLs(stdscr):
     list_ls = ls(ourPwd)
     strings = list_ls
     row_num = len( strings )
+    boxChild = drawChild(stdscr, strings[0])
     
     
-    
-
     statusbarstr = "Press 'q' to exit | STATUS BAR "
-    # Render status bar
+
+    # Render Top Status bar
+    userName = os.environ['USER']
+    hostName = socket.gethostname()
+    statusPwd=ourPwd
+    if statusPwd.startswith("/home/"+userName):
+        statusPwd = statusPwd.replace("/home/"+userName, "~")
+    stdscr.addstr(0, 2, userName+"@"+ hostName+" " +statusPwd+ "/" + strings[0])
+    
+    
+    # Render Bottom Status bar
     stdscr.attron(curses.color_pair(3))
-    stdscr.addstr(height-1, 0, statusbarstr)
+    stdscr.addstr(height-1, 0, statusbarstr)    #statusbarstr
     stdscr.addstr(height-1, len(statusbarstr), " " * (width - len(statusbarstr) - 1))
     stdscr.attroff(curses.color_pair(3))
     
@@ -175,11 +55,13 @@ def drawPwdLs(stdscr):
 
     stdscr.refresh()
     box.refresh()
+    boxParent.refresh()
+    boxChild.refresh()
     x = stdscr.getch()
 
     while x != ord('q'):
                  
-        if x == curses.KEY_DOWN:
+        if x == curses.KEY_DOWN and not row_num == 0:
             if page == 1:
                 if position < i:
                     position = position + 1
@@ -196,7 +78,8 @@ def drawPwdLs(stdscr):
                 else:
                     page = page + 1
                     position = 1 + ( max_row * ( page - 1 ) )
-        if x == curses.KEY_UP:
+            refreshChild(boxChild, ourPwd, stdscr, strings[position -1])      #refreshChild(boxChild,ourPwd,stdscr,selectedDir)
+        if x == curses.KEY_UP and not row_num == 0:
             if page == 1:
                 if position > 1:
                     position = position - 1
@@ -206,31 +89,54 @@ def drawPwdLs(stdscr):
                 else:
                     page = page - 1
                     position = max_row + ( max_row * ( page - 1 ) )
-        
-        if x == curses.KEY_LEFT:            
-            # eğer / daysak sol a gitmesini engelle
-            ourPwd = ourPwd[:ourPwd.rfind("/")]
+            refreshChild(boxChild, ourPwd, stdscr, strings[position -1])      #refreshChild(boxChild,ourPwd,stdscr,selectedDir)
 
-            if ourPwd:
+  
+        if x == curses.KEY_LEFT:    #sol oka basıldığında olanlar vs        
+            
+            ourPwd = ourPwd[:ourPwd.rfind("/")] #pathin son slashtan sonrasını sil oraya git.
+
+            if not ourPwd or len(ourPwd)<2:     #pathhte ki sorunları giderme
                 ourPwd = '/'
                 
             list_ls = ls( ourPwd )
             strings = list_ls
             row_num = len( strings )
-            stdscr.refresh()
-            box.refresh()
             
-
-        if x == curses.KEY_RIGHT:
-            ourPwd += '/' + strings[position -1]
-            list_ls = ls( ourPwd )
-            strings = list_ls
-            row_num = len( strings )
-            stdscr.refresh()
-            box.refresh()
+            #refreshParent return degeri position'a verildi. Dizinden devam etmek için.
+            #todo : position değeri dizinden devam ettirilecek.
+            
             pages = int( ceil( row_num / max_row ) )
             position = 1
             page = 1
+            
+            refreshParent(boxParent,ourPwd,stdscr) #[:ourPwd.rfind("/")]
+            refreshChild(boxChild, ourPwd, stdscr, strings[position -1])      #refreshChild(boxChild,ourPwd,stdscr,selectedDir)
+            box.refresh()
+            stdscr.refresh()
+
+        if x == curses.KEY_RIGHT and not row_num == 0 and not os.path.isfile(strings[position-1]):
+            
+                if not ourPwd or len(ourPwd)<2:     #pathhte ki sorunları giderme
+                    ourPwd = "/"+strings[position -1]
+                else:
+                    ourPwd += '/' + strings[position -1]
+                strings = ls( ourPwd )
+                row_num = len( strings )
+                
+                pages = int( ceil( row_num / max_row ) )
+                position = 1
+                page = 1
+                if row_num == 0:
+                    
+                    boxChild.erase()
+                    boxChild.refresh()
+                else:
+                    refreshChild(boxChild, ourPwd,stdscr ,strings[position -1])      #refreshChild(boxChild,ourPwd,stdscr,selectedDir)
+                
+                refreshParent(boxParent,ourPwd,stdscr) #[:ourPwd.rfind("/")]
+                box.refresh()
+                stdscr.refresh()
 
         
         if x == ord( "\n" ) and row_num != 0:
@@ -239,8 +145,10 @@ def drawPwdLs(stdscr):
             stdscr.addstr( 14, 3, "YOU HAVE PRESSED '" + strings[ position - 1 ] + "' ON POSITION " + str( position ) )
 
         box.erase()
+        
         #stdscr.border( 0 )
         box.border( 0 )
+        
 
         for i in range( 1 + ( max_row * ( page - 1 ) ), max_row + 1 + ( max_row * ( page - 1 ) ) ):
             if row_num == 0:
@@ -252,9 +160,18 @@ def drawPwdLs(stdscr):
                     box.addstr( i - ( max_row * ( page - 1 ) ), 2, str( i ) + " - " + strings[ i - 1 ], normalText )
                 if i == row_num:
                     break
+        statusPwd=ourPwd
+        if statusPwd.startswith("/home/"+userName):
+                statusPwd = statusPwd.replace("/home/"+userName, "~")
+        #todo: statıs
+        if row_num != 0:
+            stdscr.addstr(0, 2, userName+"@"+ hostName+" " +statusPwd + "/"+strings[position -1]+"\t\t\t\t\t" )  
+        else:
+            stdscr.addstr(0, 2, userName+"@"+ hostName+" " +statusPwd + "/"+"\t\t\t\t\t\t\t" )  
+        
+            
 
-
-        # Render status bar
+        # Render Bottom Status bar
         stdscr.attron(curses.color_pair(3))
         stdscr.addstr(height-1, 0, statusbarstr)
         stdscr.addstr(height-1, len(statusbarstr), " " * (width - len(statusbarstr) - 1))
